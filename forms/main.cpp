d107aa3bbaabdba6f87a6f43b52d1f67365aae15
Eloy Adonis Colell <eloy.colell@gmail.com>
2009-10-01 14:34:41 -0300

    Se modifica el main.cpp.

--- forms/main.cpp
+++ forms/main.cpp
@@ -5,6 +5,8 @@ int main( int argc, const char* argv[] ) {
 	BasicForm *f1 = new BasicForm();
 	ComplexForm *f2 = new ComplexForm();
 	Form * f3 = f1;
+	delete f1; // Borra a f3 tambien.
+	delete f2;
 	return 0;
 };
 
